4fd4136f3749f585a40de3f7dec32311df0ffb8f
clammy424 <clammy424@gmail.com>
2026-08-17 22:00:22 -0400

    setup

---
+++ server/src/server.js
@@ -0,0 +1,10 @@
+const app = require("./app");
+const connectDB = require("./config/db");
+
+const PORT = process.env.PORT || 5000;
+
+connectDB();
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
